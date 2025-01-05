07417273c601c22006a8585cc2439d15e1e943ad
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2025-01-05 18:26:16 +0530

    session year routes modifcation

--- routes/sessionYearRoutes.js
+++ routes/sessionYearRoutes.js
@@ -4,9 +4,9 @@ const { verifyToken, roleCheck } = require('../middlewares/authMiddleware');
 
 const router = express.Router();
 
-router.post('/create_session_year', verifyToken, roleCheck('admin'), createSessionYear);
-router.get('/session_years', verifyToken, roleCheck('admin'), getSessionYears);
-router.put('/update_session_year/:id', verifyToken, roleCheck('admin'), updateSessionYear);
-router.delete('/delete_session_year/:id', verifyToken, roleCheck('admin'), deleteSessionYear);
+router.post('/create-session-year', verifyToken, roleCheck('admin'), createSessionYear);
+router.get('/session-years', verifyToken, roleCheck('admin'), getSessionYears);
+router.put('/update-session-year/:id', verifyToken, roleCheck('admin'), updateSessionYear);
+router.delete('/delete-session-year/:id', verifyToken, roleCheck('admin'), deleteSessionYear);
 
 module.exports = router;
