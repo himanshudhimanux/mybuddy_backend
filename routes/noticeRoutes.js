7d0010e47c7b2bd54efd400c78113039a32b4b9a
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2025-04-06 19:23:17 +0530

    single notice route

--- routes/noticeRoutes.js
+++ routes/noticeRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { verifyToken, roleCheck } = require('../middlewares/authMiddleware');
-const { createNotice, getAllNotice, updateNotice, deleteNotice } = require('../controllers/noticeController');
+const { createNotice, getAllNotice, updateNotice, deleteNotice, singleNotice } = require('../controllers/noticeController');
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.post('/create_notice', verifyToken, roleCheck('admin'), createNotice);
 router.get('/notices', verifyToken, getAllNotice);
 router.put('/update_notice/:id', verifyToken, roleCheck('admin'), updateNotice);
 router.delete('/delete_notice/:id', verifyToken, roleCheck('admin'), deleteNotice);
+
+router.get('/single_notice/:id', verifyToken, roleCheck('admin'), singleNotice);
 
 module.exports = router;
