62596457daa0a909a189494b605433aa47d48207
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2025-06-18 23:09:04 +0530

    teacher route update

--- routes/teacherRoutes.js
+++ routes/teacherRoutes.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const { verifyToken, roleCheck } = require('../middlewares/authMiddleware');
-const { teacherRegister, getAllTeachers, specificTeacher, upload } = require('../controllers/teacherController');
+const { teacherRegister, getAllTeachers, specificTeacher, upload, deleteTeacher, updateTeacher } = require('../controllers/teacherController');
 const router = express.Router();
 
 router.post('/teacher', upload.single("photo"), verifyToken, roleCheck('admin'), teacherRegister);
+
 router.get('/teachers', verifyToken, roleCheck('admin'), getAllTeachers);
+
+router.put('/update/teacher/:id', verifyToken, roleCheck('admin'), updateTeacher);
+
+router.delete('/delete/teacher/:id', verifyToken, roleCheck('admin'), deleteTeacher);
+
 router.get('/teacher/:id', verifyToken, roleCheck('admin'), specificTeacher);
 
 
