3219064afeca7129ebd39d2ea31a9a4bd08484a0
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2025-06-18 23:22:32 +0530

    teacher modal update

--- models/Teacher.js
+++ models/Teacher.js
@@ -1,17 +1,31 @@
 const mongoose = require("mongoose");
 
-
-const teacherSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  address: { type: String, required: true },
-  subject: { type: String, required: true },
-  phone: { type: Number, required: true },
-  gender: { type: String, required: true },
-  photo: { type: String , default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg"},
-},
+const teacherSchema = new mongoose.Schema(
   {
-    timestamps: true
+    name: { type: String, required: true },
+
+    address: { type: String, required: true },
+
+    // Reference to Subject collection
+    subject: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Subject',
+      required: true,
+    },
+
+    phone: { type: Number, required: true },
+
+    gender: { type: String, required: true },
+
+    photo: {
+      type: String,
+      default:
+        "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+  },
+  {
+    timestamps: true,
   }
 );
 
-module.exports = mongoose.model('Teacher', teacherSchema);
+module.exports = mongoose.model("Teacher", teacherSchema);
