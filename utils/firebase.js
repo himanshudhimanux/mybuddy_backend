6b198f2384f9aeb7f797b086bc591e42c745fa70
himanshudhimanux <himanshudhiman.hd34@gmail.com>
2025-06-28 22:56:19 +0530

    update send notificaion submit fee

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,5 +1,6 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('../config/mybuddy-3073a-firebase-adminsdk-fbsvc-5f76455842.json'); // adjust path
+const serviceAccount = require('../../config/mybuddy-3073a-firebase-adminsdk-fbsvc-5f76455842.json');
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
